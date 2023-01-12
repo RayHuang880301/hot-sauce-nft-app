2a6b22be189c599025208ba09d1c979b8c3d62d6
RayHuang <ray880301@gmail.com>
2023-01-12 22:33:22 +0800

    add drawer

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,35 +1,96 @@
 import React from "react";
 import styles from "./Header.module.scss";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import { useRouter } from "next/router";
 import Link from "next/link";
-import { Image } from "@chakra-ui/react";
+import navIcon from "../../assets/navIcon.svg";
 import iconImg from "../../assets/icon.png";
+import closeIcon from "../../assets/closeIcon.svg";
 import { UrlObject } from "url";
-
-declare type Url = string | UrlObject;
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import {
+	Drawer,
+	DrawerBody,
+	DrawerCloseButton,
+	DrawerContent,
+	DrawerHeader,
+	DrawerOverlay,
+	Image,
+	useDisclosure,
+} from "@chakra-ui/react";
+import { useDeviceDetect } from "../../utils/mediaQuery";
 
 export default function Header() {
+	const { isTabletOrMobile } = useDeviceDetect();
+	const { isOpen, onOpen, onClose } = useDisclosure();
 	return (
 		<div className={styles.header}>
 			<Image src={iconImg.src} width="75px" alt="icon" />
-			<div className={styles.box}>
-				<div className={styles.selector}>
-					<Link href="#intro">
-						<div className={styles.btn}>About</div>
-					</Link>
-					<Link href="#mint">
-						<div className={styles.btn}>Mint</div>
-					</Link>
-					<Link href="#mdd">
-						<div className={styles.btn}>Team</div>
-					</Link>
-					<Link href="#faq">
-						<div className={styles.btn}>FAQs</div>
-					</Link>
+			{isTabletOrMobile ? (
+				<div className={styles.boxMobile}>
+					<ConnectButton />
+					<Image
+						className={styles.navIcon}
+						src={navIcon.src}
+						onClick={onOpen}
+						width="45px"
+						alt="icon"
+					/>
 				</div>
-				<ConnectButton />
-			</div>
+			) : (
+				<div className={styles.box}>
+					<div className={styles.selector}>
+						<Link href="#intro">
+							<div className={styles.btn}>About</div>
+						</Link>
+						<Link href="#mint">
+							<div className={styles.btn}>Mint</div>
+						</Link>
+						<Link href="#mdd">
+							<div className={styles.btn}>Team</div>
+						</Link>
+						<Link href="#faq">
+							<div className={styles.btn}>FAQs</div>
+						</Link>
+					</div>
+					<ConnectButton />
+				</div>
+			)}
+
+			<Drawer onClose={onClose} isOpen={isOpen} size={"full"}>
+				<DrawerOverlay className={styles.drawerContent} />
+				<DrawerContent>
+					<DrawerHeader className={styles.drawerHeader}>
+						<Image src={iconImg.src} width="100px" alt="icon" />
+						<Image
+							src={closeIcon.src}
+							width="20px"
+							alt="icon"
+							onClick={onClose}
+						/>
+					</DrawerHeader>
+					<DrawerBody className={styles.drawerBody}>
+						<Link href="#intro">
+							<div className={styles.btn} onClick={onClose}>
+								About
+							</div>
+						</Link>
+						<Link href="#mint">
+							<div className={styles.btn} onClick={onClose}>
+								Mint
+							</div>
+						</Link>
+						<Link href="#mdd">
+							<div className={styles.btn} onClick={onClose}>
+								Team
+							</div>
+						</Link>
+						<Link href="#faq">
+							<div className={styles.btn} onClick={onClose}>
+								FAQs
+							</div>
+						</Link>
+					</DrawerBody>
+				</DrawerContent>
+			</Drawer>
 		</div>
 	);
 }
