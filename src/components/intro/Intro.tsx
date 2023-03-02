f2326c2be97e847de373af5b232bf7f51bfe7658
abner <fantasrulai@gmail.com>
2023-03-02 19:15:53 +0800

    update marquee

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -9,11 +9,41 @@ export default function Intro() {
 			<div></div>
 			<div className={styles.container}>
 				{/* <Image className={styles.img} src={rectImg.src} alt="rectangle" /> */}
-				<video className={styles.img} controls autoPlay loop muted>
-					<source src="/nft.mov" type="video/mp4"></source>
-				</video>
+				<div className={styles.videoBox}>
+
+						<video
+								className={styles.videoItem}
+								controls="controls"
+								autoPlay="autoPlay"
+								loop="loop"
+								muted="muted">
+								<source src="/nft.mov" type="video/mp4"></source>
+						</video>
+						<div className={styles.videoMarquee}>
+							<div className={styles.videoMarqueeItem}>
+								<img src="/nft1.gif"/>
+								<img src="/nft2.gif"/>
+								<img src="/nft3.gif"/>
+								<img src="/nft4.gif"/>
+								<img src="/nft1.gif"/>
+								<img src="/nft2.gif"/>
+								<img src="/nft3.gif"/>
+								<img src="/nft4.gif"/>
+							</div>
+							<div className={styles.videoMarqueeItem}>
+								<img src="/nft4.gif"/>
+								<img src="/nft3.gif"/>
+								<img src="/nft2.gif"/>
+								<img src="/nft1.gif"/>
+								<img src="/nft4.gif"/>
+								<img src="/nft3.gif"/>
+								<img src="/nft2.gif"/>
+								<img src="/nft1.gif"/>
+							</div>
+						</div>
+				</div>
 				<div className={styles.box}>
-					<h1>What is the HOT SAUCE NFT</h1>
+					<h1>What is the HOT SAUCE NFT?</h1>
 					<p>
 						The HOT SAUCE NFT is a digital non-fungible token (NFT) representing
 						MDD’s newest EP HOT SAUCE. Each NFT features unique, hand-drawn
